e353014af8322cf05f89e05e87bb3705b240dfcf
Frédéric <alphonse.frederic23@icloud.com>
2022-12-05 11:30:42 +0100

    Initial Commit

---
+++ Morpion Game/Morpion_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Morpion_GameApp.swift
+//  Morpion Game
+//
+//  Created by Frédéric ALPHONSE on 05/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct Morpion_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
