4015b28b82841f8ac04d4f8044ee9c69785bfd8b
Frédéric <alphonse.frederic23@icloud.com>
2022-12-05 22:10:16 +0100

    Ajout des symboles sur la grille du morion. Plus des alignements de chacunes des cases avec différents spacers et padding. Ajout des animations de changements de symbole

--- Morpion Game/ContentView.swift
+++ Morpion Game/ContentView.swift
@@ -8,12 +8,111 @@
 import SwiftUI
 
 struct ContentView: View {
+    var square_sf = "square.circle.fill"
+    var circle_sf = "circle.circle.fill"
+    var blank_sf = "pencil"
+    @State var object = "pencil"
+    @State var counter = 0
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Button("Morpion Game") {
+                switch counter {
+                case 0:
+                    object = square_sf
+                    counter += 1
+                case 1:
+                    object = circle_sf
+                    counter += 1
+                case 2:
+                    object = blank_sf
+                    counter += 1
+                default:
+                    object = blank_sf
+                    counter = 0
+                }
+            }
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .foregroundColor(Color.blue)
+                .padding(15.0)
+            Spacer()
+            ZStack {
+                HStack{
+                    
+                    
+                    VStack {
+                        Image(systemName:object)
+                        Spacer()
+                        Image(systemName:object)
+                        Spacer()
+                        Image(systemName:object)
+                        Spacer()
+                    }
+                    
+                    Spacer()
+                        .frame(width: 90.0)
+                        
+                    
+                    VStack {
+                        Image(systemName:object)
+                        Spacer()
+                        Image(systemName:object)
+                        Spacer()
+                        Image(systemName:object)
+                        Spacer()
+                    }
+                    Spacer()
+                        .frame(width: 90.0)
+                    
+                    VStack {
+                        Image(systemName:object)
+                        Spacer()
+                        Image(systemName:object)
+                        Spacer()
+                        Image(systemName:object)
+                        Spacer()
+                    }
+                    
+                    
+                }
+                .padding(.top, 90.0)
+                .frame(width: 350.0, height: 400.0)
+                Image("Grid Morpion")
+                    .resizable()
+                    //.padding(.bottom, 30.0)
+                    .frame(width: 350.0, height: 400.0)
+                
+                
+            }
+            .padding(.top)
+           
+            Spacer()
+            
+            HStack {
+                Spacer()
+                VStack {
+                    
+                    Text("Player")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                    Text("0")
+                        .font(.title)
+                    
+                    Spacer()
+                }
+                Spacer()
+                VStack {
+                    Text("CPU")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                    Text("0")
+                        .font(.title)
+                    Spacer()
+                }
+                Spacer()
+            }
+            
+            Spacer()
         }
         .padding()
     }
